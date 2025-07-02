b7b8a6ea3c3573c5711106ebfb465bd68b80d3d6
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-02 16:38:05 +0700

    feat(config): add logger

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,55 @@
-use anyhow::Result;
 use serde::Deserialize;
 use std::fs;
 
-#[derive(Deserialize)]
-pub struct Config {}
+#[derive(Deserialize, Default)]
+pub struct Config {
+    #[serde(default)]
+    pub logger: Logger,
+}
 
 impl Config {
-    pub fn read_file<'a>(path: &'a str) -> Result<Self> {
+    pub fn read_file<'a>(path: &'a str) -> anyhow::Result<Self> {
         let content = fs::read_to_string(path)?;
         let config = toml::from_str::<Self>(&content)?;
         return Ok(config);
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Logger {
+    #[serde(default = "logger::level::default", with = "logger::level")]
+    pub level: log::LevelFilter,
+}
+
+mod logger {
+    pub mod level {
+        use serde::{Deserialize, Deserializer};
+        use std::str::FromStr;
+
+        // NOTE: based on private log::LOG_LEVEL_NAMES
+        static LOG_LEVEL_NAMES: [&str; 6] = ["OFF", "ERROR", "WARN", "INFO", "DEBUG", "TRACE"];
+
+        pub fn default() -> log::LevelFilter {
+            return log::LevelFilter::Info;
+        }
+
+        pub fn deserialize<'de, D>(deserializer: D) -> Result<log::LevelFilter, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            let value = String::deserialize(deserializer)?;
+            let value = log::LevelFilter::from_str(value.as_str())
+                .map_err(|_| serde::de::Error::unknown_variant(value.as_str(), &LOG_LEVEL_NAMES))?;
+
+            return Ok(value);
+        }
+    }
+}
+
+impl Default for Logger {
+    fn default() -> Self {
+        return Logger {
+            level: logger::level::default(),
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ mod config;
 
 use anyhow::Result;
 use config::Config;
+use simple_logger::SimpleLogger;
+
+const CONFIG_PATH: &str = "./config.toml";
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
-    let config = Config::read_file("./config.toml")?;
+    SimpleLogger::new().init()?;
+
+    let config = Config::read_file(CONFIG_PATH)?;
+    log::set_max_level(config.logger.level);
+
+    log::info!("testing");
 
     return Ok(());
 }
