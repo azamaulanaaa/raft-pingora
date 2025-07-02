c14ce66c4fcd3bb126340ef83046345734f1ea89
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-02 16:46:40 +0700

    chore: load config should be earlier

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use simple_logger::SimpleLogger;
 static CONFIG_PATH: &str = "./config.toml";
 
 fn main() -> Result<()> {
-    SimpleLogger::new().init()?;
-
     let config = Config::read_file(CONFIG_PATH)?;
+
+    SimpleLogger::new().init()?;
     log::set_max_level(config.logger.level);
 
     log::info!("testing");
