f747d2e57c3df895f57b7933ade343e51be005b1
Aza Maulana <azamaulanaaa@gmail.com>
2025-07-04 15:42:15 +0700

    feat(clock): add periodic call via clock

---
+++ src/clock.rs
@@ -0,0 +1,86 @@
+use anyhow::{Result, anyhow};
+use futures::FutureExt;
+use std::{future::Future, sync::Arc, time::Duration};
+use tokio::{sync::oneshot, task::JoinHandle, time};
+
+pub struct Clock<F, T>
+where
+    F: Fn() -> T + Send + Sync + 'static,
+    T: Future<Output = ()> + Send + 'static,
+{
+    interval: Duration,
+    callback: Arc<F>,
+    task_handle: Option<JoinHandle<()>>,
+    shutdown_tx: Option<oneshot::Sender<()>>,
+}
+
+impl<F, T> Clock<F, T>
+where
+    F: Fn() -> T + Send + Sync + 'static,
+    T: Future<Output = ()> + Send + 'static,
+{
+    pub fn new(interval: Duration, callback: F) -> Self {
+        Clock {
+            interval,
+            callback: Arc::new(callback),
+            task_handle: None,
+            shutdown_tx: None,
+        }
+    }
+
+    pub fn start(&mut self) -> Result<()> {
+        if self.task_handle.is_some() {
+            return Err(anyhow!("clock is already running"));
+        }
+
+        let interval = self.interval;
+        let callback = Arc::clone(&self.callback);
+
+        let mut interval = time::interval(interval);
+        interval.set_missed_tick_behavior(time::MissedTickBehavior::Skip);
+
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        self.shutdown_tx = Some(shutdown_tx);
+        let mut fused_shutdown_rx = shutdown_rx.fuse();
+
+        let handle = tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    _ = interval.tick() => callback().await,
+                    _ = &mut fused_shutdown_rx => break,
+                };
+            }
+        });
+
+        self.task_handle = Some(handle);
+
+        Ok(())
+    }
+
+    pub fn stop(&mut self) -> Result<()> {
+        match self.shutdown_tx.take() {
+            None => return Err(anyhow!("clock is not running")),
+            Some(tx) => tx
+                .send(())
+                .map_err(|_| anyhow!("something wrong when stopping clock"))?,
+        };
+
+        self.task_handle = None;
+
+        Ok(())
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.task_handle.as_ref().is_some_and(|h| !h.is_finished())
+    }
+
+    pub async fn wait(&mut self) -> Result<()> {
+        match self.task_handle.take() {
+            None => return Err(anyhow!("clock is not running")),
+            Some(handle) => handle.await?,
+        };
+
+        self.task_handle = None;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+mod clock;
 mod config;
 mod raft;
 
-use crate::{config::Config, raft::Raft};
+use crate::{clock::Clock, config::Config, raft::Raft};
 use anyhow::Result;
 use simple_logger::SimpleLogger;
 
@@ -15,6 +16,14 @@ async fn main() -> Result<()> {
     log::set_max_level(config.logger.level);
 
     let raft = Raft::new(&config.raft)?;
+
+    let mut clock = Clock::new(std::time::Duration::from_secs(1), async || {
+        log::info!("Hello world!");
+    });
+    clock.start()?;
+    clock.wait();
+    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    clock.stop()?;
 
     Ok(())
 }
